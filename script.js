0ddae2ce70bc3cf4cfe6b7fc05b9f9f09b8af43c
Tuan Anh Pham <tuananh@hotmail.se>
2022-12-03 18:07:43 +0100

    successfully merge Asho's code

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ const text = document.querySelector('#text');
 function saveNote(){
     let titleInput = title.value
     let textInput = text.value
-    let dataInput = date.value
+    let dateInput = date.value
     let notes = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [];
-    let newNode = {titleInput,textInput,dataInput};
+    let newNode = {titleInput,textInput,dateInput};
     notes.push(newNode)
     localStorage.setItem('notes', JSON.stringify(notes))
     renderNote()
+    clear()
 }
 // hämtar data från local storage och renderar till notes-list
 function renderNote(){
@@ -25,7 +26,9 @@ function renderNote(){
             <p>${value.textInput.substring(0,max__text__strings)}
                 ${value.textInput.length > max__text__strings ? '...' : ''}
             </p>
-            <p>${value.dataInput}</p>
+            <p>${value.dateInput}</p>
+            <button onclick = 'deleteNotes(${index})'>delete</button>
+            <button class = 'fav-btn' onclick = 'favoriteNotes(${index})'>fav</button>
         </li>
     `
     })
@@ -38,6 +41,36 @@ function updateNotes(index){
     text.value = notes[index].textInput;
     date.value = notes[index].dataInput;
 }
+function clearNotes(){
+    title.value ='';
+    text.value ='';
+    date.value ='';
+}
+function deleteNotes(index){
+    let notes = localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : [];
+    if(confirm('Are you sure you want to delete thhis note?')){
+        notes.splice(index,1)
+    }
+    localStorage.setItem('notes',JSON.stringify(notes));
+    renderNote()
+}
+/* function favoriteNotes(index){
+    document.querySelector('.fav-btn').classList.toggle('btn-color')
+} */
+
+const open = document.querySelector('.section-logo');
+const modal = document.querySelector('.modal-container');
+const close = document.querySelector('.close');
+console.log(open);
+
+function toggle(){
+    modal.classList.toggle('hide');
+}
+open.addEventListener('click',toggle);
+close.addEventListener('click',toggle)
+
+
+
 
 
 
